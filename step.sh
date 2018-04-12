3c59753d033ba8fded3fae179022a00dae838312
nsimonov <nikolay.simonov@harman.com>
2018-04-12 11:41:17 +0300

    add output

--- step.sh
+++ step.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 set -ex
 
-FLUTTER_ANDROID_PROJECT="`pwd`/android/"
+FLUTTER_ANDROID_PROJECT=`pwd`/android
 if [[ -d "$FLUTTER_ANDROID_PROJECT" ]]
 then
-    if [[ -d "`pwd`/flutter/" ]]
+    export FLUTTER_HOME=`pwd`/flutter
+    envman add --key FLUTTER_HOME --value $FLUTTER_HOME
+    envman run bash -c 'echo "fluter home env is: $FLUTTER_HOME"'
+        
+    export PATH=$FLUTTER_HOME/bin:$PATH
+    envman add --key PATH --value $PATH
+    
+    if [[ -d $FLUTTER_HOME ]]
     then
         echo 'flutter already cloned'
-        export PATH=`pwd`/flutter/bin:$PATH
-        envman add --key PATH --value $PATH
-        
         flutter upgrade
     else
         git clone https://github.com/flutter/flutter.git -b ${flutter_branch}
-        export PATH=`pwd`/flutter/bin:$PATH
-        envman add --key PATH --value $PATH
     fi
     
     flutter doctor
 
-    export FLUTTER_SDK_PROP=flutter.sdk=`pwd`/flutter
+    export FLUTTER_SDK_PROP=flutter.sdk=$FLUTTER_HOME
                         
-    echo $FLUTTER_SDK_PROP >> `pwd`/android/local.properties
+    echo $FLUTTER_SDK_PROP >> $FLUTTER_ANDROID_PROJECT/local.properties
     
     echo ${debug_build}
     
     if [[ ${debug_build} = "yes" ]]
     then
-        echo 'flutter.buildMode=debug' >> `pwd`/android/local.properties
+        echo 'flutter.buildMode=debug' >> $FLUTTER_ANDROID_PROJECT/local.properties
     fi
     exit 0
 else
